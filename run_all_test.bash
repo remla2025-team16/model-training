0d5637ade2fef5b315f7531762ef6ea156267312
Pratham Johari <pratham244200@gmail.com>
2025-05-27 00:18:38 +0200

    Test script bug fix, uncomment

--- run_all_test.bash
+++ run_all_test.bash
@@ -1,8 +1,9 @@
-#set -e
+set -e
 echo "=== Running pytest with coverage ==="
-pytest --cov=src --cov-report=term-missing
+pytest --cov=src --cov-report=term-missing > test_report.txt
 
 COV=$(coverage report | tail -n 1 | awk '{print $4}')
+
 echo ">>> Coverage: ${COV}"
 
 echo "=== Running mutation testing (mutmut) ==="
@@ -10,7 +11,14 @@ mutmut run src
 MUT_SCORE=$(mutmut results | grep ^Score | awk '{print $2}')
 echo ">>> Mutation Testing Score: ${MUT_SCORE}"
 
-echo "All tests passed with coverage ${COV} and mutation score ${MUT_SCORE}."
+BENCH_LINE=$(grep 'test_inference_latency' test_report.txt)
+
+MIN_LAT=$(echo $BENCH_LINE | awk '{print $2}' | tr -d ',')
+MAX_LAT=$(echo $BENCH_LINE | awk '{print $3}' | tr -d ',')
+MEAN_LAT=$(echo $BENCH_LINE | awk '{print $4}' | tr -d ',')
+
+echo ">>> Latency - Min: $MIN_LAT µs, Max: $MAX_LAT µs, Mean: $MEAN_LAT µs"
+
 
 if [ ! -d "metrics" ]; then
   echo "Creating metrics directory..."
@@ -19,9 +27,17 @@ else
   echo "Metrics directory already exists."
 fi
 
+
 cat <<EOF > metrics/test_metrics.json
 {
   "coverage": "${COV}",
-  "mutation_score": "${MUT_SCORE}"
+  "mutation_score": "${MUT_SCORE}",
+  "latency": {
+    "min_us": ${MIN_LAT},
+    "max_us": ${MAX_LAT},
+    "mean_us": ${MEAN_LAT}
+  }
 }
 EOF
+
+rm test_report.txt
